efc3b329792a915d05efa5c96a317fd804acf157
Joseph9720 <Josephj9720@gmail.com>
2025-08-30 01:54:40 -0400

    (feat): Added idempotency key checks for deposit and withdraw operations
    - The methods from the TransactionService now check if the client has
      sent and idempotency key, search for it in the db and if is is found,
      return a generic (idempotent) result. If it was not found in the db,
      they instead record the key in the database along with the id of the
      user who initiated the transaction.

--- backend/src/main/java/dev/jordanjoseph/backend/service/TransactionService.java
+++ backend/src/main/java/dev/jordanjoseph/backend/service/TransactionService.java
@@ -2,6 +2,7 @@ package dev.jordanjoseph.backend.service;
 
 import dev.jordanjoseph.backend.dto.AccountView;
 import dev.jordanjoseph.backend.model.Account;
+import dev.jordanjoseph.backend.model.IdempotencyKey;
 import dev.jordanjoseph.backend.repository.AccountRepository;
 import dev.jordanjoseph.backend.repository.IdempotencyKeyRepository;
 import dev.jordanjoseph.backend.repository.TransactionRepository;
@@ -29,21 +30,62 @@ public class TransactionService {
     private AccountValidator accountValidator;
 
     @Transactional
-    public AccountView deposit(UUID accountId, BigDecimal amount) {
+    public AccountView deposit(UUID accountId, BigDecimal amount, String idemKey) {
+
         Account account = accountValidator.exist(accountId);
-        accountValidator.requireOwned(account.getUser().getId());
+
+        UUID ownerId = account.getUser().getId();
+        accountValidator.requireOwned(ownerId);
+
+        //idemKey sent from client, if not present, continue as if new operation
+        if(idemKey != null && !idemKey.isBlank()) {
+            if(idempotencyKeyRepository.existsByOwnerIdAndKeyValue(ownerId, idemKey)) {
+                //operation already processed - return a generic (idempotent) result
+                return new AccountView(account.getId(), account.getBalance());
+            }
+        }
+
         accountValidator.requirePositive(amount);
         account.setBalance(account.getBalance().add(amount));
+
+        //record idempotency after success - if it was sent by client
+        if(idemKey != null && !idemKey.isBlank()) {
+            IdempotencyKey key = new IdempotencyKey();
+            key.setOwnerId(ownerId);
+            key.setKeyValue(idemKey);
+            idempotencyKeyRepository.save(key);
+        }
+
         return new AccountView(account.getId(), account.getBalance());
     }
 
     @Transactional
-    public AccountView withdraw(UUID accountId, BigDecimal amount) {
+    public AccountView withdraw(UUID accountId, BigDecimal amount, String idemKey) {
+
         Account account = accountValidator.exist(accountId);
-        accountValidator.requireOwned(account.getUser().getId());
+
+        UUID ownerId = account.getUser().getId();
+        accountValidator.requireOwned(ownerId);
+
+        if(idemKey != null && !idemKey.isBlank()) {
+            if(idempotencyKeyRepository.existsByOwnerIdAndKeyValue(ownerId, idemKey)) {
+                //operation already processed - return generic (idempotent) result
+                return new AccountView(account.getId(), account.getBalance());
+            }
+        }
+
         accountValidator.requirePositive(amount);
         accountValidator.requireSufficientFunds(account.getBalance(), amount);
         account.setBalance(account.getBalance().subtract(amount));
+
+        //record idempotence after success - if sent by client
+        if(idemKey != null && !idemKey.isBlank()) {
+            IdempotencyKey key = new IdempotencyKey();
+            key.setOwnerId(ownerId);
+            key.setKeyValue(idemKey);
+            idempotencyKeyRepository.save(key);
+        }
+
         return new AccountView(account.getId(), account.getBalance());
     }
 

6b255cd3dca348597f588023a22c414e9a4a1a39
Joseph9720 <Josephj9720@gmail.com>
2025-08-30 03:45:32 -0400

    feat: Created TransactionController
    - The TransactionController is responsible for transaction related
      enpoints. The endpoints for extracting the transactions from an
      account or to view a specific transaction are not yet implemented.
      Only the route was mapped. The route for transfers has been mapped
      and the response is generated through the TransactionService.
    
    - The enpoint for transfers is in the TransactionController and
      not in the AccountController because it could be used for multiple
      accounts that the user could have in the future. For the MVP, the
      user currently can only have on account. This choice for the endpoint
      allows for future extension.
    
    - Acutally implemented the transaction record creation for the
      withdraw and deposit methods. The first time it was written
      into a commit without being fully implemented.
    
    **Key Points**
    - Transfer functionality fully operational (MVP).
    
    Closes #7

---
+++ backend/src/main/java/dev/jordanjoseph/backend/controller/TransactionController.java
@@ -0,0 +1,41 @@
+package dev.jordanjoseph.backend.controller;
+
+import dev.jordanjoseph.backend.dto.TransferRequest;
+import dev.jordanjoseph.backend.dto.TransferResponse;
+import dev.jordanjoseph.backend.model.Transaction;
+import dev.jordanjoseph.backend.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping("/accounts/{account_id}/transactions")
+    public ResponseEntity<List<Transaction>> getAccountTransactions(@PathVariable UUID accountId) {
+
+        return null; //implement later.
+    }
+
+    @GetMapping("/transactions/{transaction_id}")
+    public ResponseEntity<Transaction> getTransaction(@PathVariable UUID transactionId) {
+
+        return null; //implement later
+    }
+
+    //putting it here allows for extension with future support for multiple accounts per user
+    @PostMapping("/transactions/transfer")
+    public ResponseEntity<TransferResponse> transfer(
+            @RequestBody @Valid TransferRequest request,
+            @RequestHeader(value = "Idempotency-Key", required = false) String idemKey) {
+        TransferResponse response = transactionService.transfer(request, idemKey);
+        return ResponseEntity.ok(response);
+    }
+}

e9276370a23aa2bb666363ed7ae5fc4c493a07bb
Joseph9720 <Josephj9720@gmail.com>
2025-09-01 05:07:06 -0400

    feat: Added new methods in TransactionRepository for sorting
    - Added four methods to search Transactions by accound id, account id and type,
      by account id and between two instants, and finally, by account id, type, and
      between two instants.

--- backend/src/main/java/dev/jordanjoseph/backend/repository/TransactionRepository.java
+++ backend/src/main/java/dev/jordanjoseph/backend/repository/TransactionRepository.java
@@ -13,4 +13,12 @@ import java.util.UUID;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     List<Transaction> findByReferenceAndByAccountIdNot(String reference, UUID accountId);
+
+    Page<Transaction> findByAccountId(UUID accountId, Pageable pageable);
+
+    Page<Transaction> findByAccountIdAndType(UUID accountId, Transaction.Type type, Pageable pageable);
+
+    Page<Transaction> findByAccountIdAndCreatedAtBetween(UUID accountId, Instant from, Instant to, Pageable pageable);
+
+    Page<Transaction> findByAccountIdAndTypeAndCreatedAtBetween(UUID accountId, Transaction.Type type, Instant from, Instant to, Pageable pageable);
 }

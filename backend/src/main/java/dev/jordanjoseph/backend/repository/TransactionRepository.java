f5e47d5ce33418cfae010bfb5788a36432ccd757
Joseph9720 <Josephj9720@gmail.com>
2025-09-01 05:03:21 -0400

    refactor: modified unused method from TransactionRepository
    - method findAllByReference was not used yet.
      There is no proper use of that method for a User that is not admin.
      Admin already has access to Id, so it doesn't need that specific
      method. Transfers can only be between two parties so there can only
      be at most one other Transaction that has the same reference.
      So changed this method for a method that find's a transaction by
      reference and excludes the currently known accound Id. So only
      the Transaction that shares a reference with the current one is
      found.

--- backend/src/main/java/dev/jordanjoseph/backend/repository/TransactionRepository.java
+++ backend/src/main/java/dev/jordanjoseph/backend/repository/TransactionRepository.java
@@ -1,13 +1,16 @@
 package dev.jordanjoseph.backend.repository;
 
 import dev.jordanjoseph.backend.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-    List<Transaction> findAllByReference(String reference);
+    List<Transaction> findByReferenceAndByAccountIdNot(String reference, UUID accountId);
 }

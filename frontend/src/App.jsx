317056f997c71cddf7a8f1b04edcea7d89393562
Joseph9720 <Josephj9720@gmail.com>
2025-09-15 13:16:34 -0400

    feat: added CSSBasline to application
    - This was done to have a consistent starting point
      for styling in the application across the different
      browsers.
    
    - index.css was already using a reset but it didn't
      apply the proper background changes for light and
      dark mode. Adding CssBaseline at the app level
      fixed that issue.
    
    - removed margin and padding properties from index.css
      in the body defaults. CSSBasline already provides it.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Router from './components/Router'
 import { ThemeProvider } from './components/ThemeProvider'
+import { CssBaseline } from '@mui/material'
 
 function App() {
 
   return (
     <>
     <ThemeProvider>
+      <CssBaseline/>
       <Router/>
     </ThemeProvider>
     </>

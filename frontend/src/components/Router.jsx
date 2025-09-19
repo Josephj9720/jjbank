4019506a134bcc9a716c31e6474109a1856ebb60
Joseph9720 <Josephj9720@gmail.com>
2025-09-18 23:52:43 -0400

    feat: Wrapped with Public and Protected Routes
    - The public routes, login and register, have been wrapped in
      a PublicRoute component. This allows to redirect the app
      to the proper page if the user is authenticated already.
    
    - The protected routes, dashboard, accounts and transfer,
      have been wrapped in a ProtectedRoute component. This
      allows to redirect the app to the login page if the
      user is not authenticated. Once logged in, the app
      will be redirected to the proper page thanks to
      the PublicRoute component.
    
    - The navigation process has been made easily reusable
      through the PublicRoute and ProtectedRoute components.

--- frontend/src/components/Router.jsx
+++ frontend/src/components/Router.jsx
@@ -4,17 +4,19 @@ import Layout from './Layout';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Dashboard  from './Pages/Dashboard';
+import PublicRoute from './PublicRoute';
+import ProtectedRoute from './ProtectedRoute';
 
 function Router() {
   return (
     <Routes>
       <Route path={FRONT_END_ROUTES.HOME} element={<Layout/>}>
         <Route index element={<p>home</p>}/> {/* index is the default content of the parent route */}
-        <Route path={FRONT_END_ROUTES.LOGIN} element={<Login/>}/>
-        <Route path={FRONT_END_ROUTES.REGISTER} element={<Register/>}/>
-        <Route path={FRONT_END_ROUTES.DASHBOARD} element={<Dashboard/>}/>
-        <Route path={FRONT_END_ROUTES.ACCOUNTS} element={<p>accounts</p>}/>
-        <Route path={FRONT_END_ROUTES.TRANSFER} element={<p>transfer</p>}/>
+        <Route path={FRONT_END_ROUTES.LOGIN} element={<PublicRoute><Login/></PublicRoute>}/>
+        <Route path={FRONT_END_ROUTES.REGISTER} element={<PublicRoute><Register/></PublicRoute>}/>
+        <Route path={FRONT_END_ROUTES.DASHBOARD} element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+        <Route path={FRONT_END_ROUTES.ACCOUNTS} element={<ProtectedRoute><p>accounts</p></ProtectedRoute>}/>
+        <Route path={FRONT_END_ROUTES.TRANSFER} element={<ProtectedRoute><p>transfer</p></ProtectedRoute>}/>
       </Route>
     </Routes>
   );

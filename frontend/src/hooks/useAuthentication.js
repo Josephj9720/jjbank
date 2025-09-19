b31cdf931e8ef3b16bacaf4e3d454bbafdbd5317
Joseph9720 <Josephj9720@gmail.com>
2025-09-18 21:47:34 -0400

    feat: Created authentication context and hooks
    - AuthenticationContext represents the context for everything
        authentication related
    
    - useAuthContext calls useContext using an AuthenticationContext
        It is used to share authentication information globally
    
    - useRefreshToken uses the information from the
        Authentication context through useAuthContext hook
        to refresh the authentication details if they don't
        already exist. It first attemps to use the refresh
        token from the cookie if it exists to get a new
        access token, the full name of the user is sent
        with the access token. If the new details are received,
        the state of the authentication details is updated.
        If the refresh attemp was failed, then the state of the
        authentication details is set to null. All of this process
        is written in a useCallBack hook so that the method can be
        memoized. This improves performance if the dependencies,
        the authDetails and setAuthDetails, have not changed. The
        callBack is then called in a useEffect hook inside of the
        useRefreshToken hook.
    
    - This is useful because the authentication can be checked
        globally now.

---
+++ frontend/src/hooks/useAuthentication.js
@@ -0,0 +1,50 @@
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { FRONT_END_ROUTES } from "../util/routes";
+import api from "../util/apiClient";
+import { API_ENDPOINTS } from "../util/endpoints";
+
+//create context object with default value
+export const AuthenticationContext = createContext();
+
+//custom hook
+//returns the prop value from the nearest context provider
+export const useAuthContext = () => useContext(AuthenticationContext);
+
+export const useRefreshToken = () => {
+
+  const [authDetails, setAuthDetails] = useState(null);
+
+  //returns a memoized version of the method unless one of the inputs has changed
+  const refreshToken = useCallback(() => {
+    //check if the authDetails object exists and has an access token, if it exists, don't refresh
+    if(authDetails && authDetails.accessToken) {
+      return;
+    }
+
+    console.log("Attempting to refresh token...");
+
+    api.post(API_ENDPOINTS.REFRESH, null, {
+      withCredentials: true //this allows to send the cookie
+    })
+    .then(response => {
+      //success: update the auth details in the global state
+      setAuthDetails({
+        fullName: response.data.fullName,
+        accessToken: response.data.accessToken,
+      });
+    })
+    .catch(error => {
+      console.log("Failed to refresh token", error);
+      //failure: clear auth details (log out user)
+      setAuthDetails(null);
+    })
+
+
+  }, [authDetails, setAuthDetails]); //dependency array to prevent re-creation
+  
+  useEffect(() => {
+    refreshToken();
+  }, [refreshToken]);
+
+  return [authDetails, setAuthDetails];
+}

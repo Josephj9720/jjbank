3a1f91ca278e0e6c750e476bd6e75200bf1dd38f
Joseph9720 <Josephj9720@gmail.com>
2025-09-25 04:05:27 -0400

    feat: Created Logout.jsx component
    - Logout.jsx is a compnent that shows a that the user is
      logging out and will be redirected.
    
    - The component displays the redirectionTime. It is set to
      5 seconds.
    
    - The redirectToHome callBack is created using useCallBack
      in order to be used inside of useEffect since useEffect
      require that method doesn't change on every render.
      Writting the method directly in the context of the
      component would cause it to be re-created on every render.
    
      The method was not written inside of useEffect directly
      because it is also used as the onClick method for the
      'Logout' button.
    
    - A timer is set inside of useEffect to update the
      redirectTime displayed on the page. The redirection
      time is a state. The setInterval method is used to
      create an interval in which the redirectTime is
      decremented every 1000ms which is 1 second. When
      the redirectTime reaches 0, the app navigates to the
      HOME page. The interval is cleared using clearInterval
      method when the app re-renders.
    
    - An api call to the logout endpoint is attempted
      in another useEffect hook. Not data is sent in the
      request body and the withCredentials option is set
      to true so that the refresh token cookie is sent
      with the request. If the api call is successful,
      a success message is printed to the console and the
      authentication details of the user are set to null.
      If the api call fails, an error message is displayed.

---
+++ frontend/src/components/Pages/Logout.jsx
@@ -0,0 +1,105 @@
+import { Button, CardContent, Typography } from "@mui/material";
+import useTitle from "../../hooks/useTitle";
+import AuthCard from "../AuthCard";
+import { useCallback, useEffect, useState } from "react";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useNavigate } from "react-router";
+import { FRONT_END_ROUTES } from "../../util/routes";
+import { useAuthContext } from "../../hooks/useAuthentication";
+import api from "../../util/apiClient";
+import { API_ENDPOINTS } from "../../util/endpoints";
+
+
+const Logout = () => {
+  useTitle("Logout | JJ Bank");
+
+  //time before user is redirected to home page
+  const [redirectTime, setRedirectTime] = useState(5);
+
+  const { setAuthDetails } = useAuthContext();
+
+  const navigate = useNavigate();
+
+  //returns a memoized version of the method, prevents dependencies of 
+  //useEffect from chaging on every render
+  //functions defined directly in the scope of a component are recreated on every render
+  const redirectToHome = useCallback(() => {
+    navigate(FRONT_END_ROUTES.HOME, {replace: true});
+  }, [navigate]);
+
+  useEffect(() => {
+    //make call to backend to logout the user
+    api.post(API_ENDPOINTS.LOGOUT, null, {
+      withCredentials: true // this allows to send the cookie
+    })
+    .then(() => {
+      //successful logout: clear auth details from context
+      console.log("successful logout");
+      setAuthDetails(null);
+    })
+    .catch(error => {
+      console.log("Failed to logout user", error);
+    });
+
+  }, [setAuthDetails]);
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      //when redirect time runs out, redirect to home page
+      if(redirectTime == 0) redirectToHome();
+
+      //decrement redirect time by 1
+      setRedirectTime(time => time - 1);
+    }, 1000);
+
+    //clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+
+    //add redirectTime as dependency to re-run effect when it is updated
+    //add redirectToHome as dependency for correctness even if unchanging
+  }, [redirectTime, redirectToHome]);
+
+  return (
+    <AuthCard>
+      <CardContent 
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "25%",
+          height: "50%",
+        }}
+      >
+        <LogoutIcon 
+          color="secondary"
+          fontSize="large"
+        />
+        <Typography
+          variant="h4"
+          marginBottom={"5%"}
+        >
+          Logging out...
+        </Typography>
+        <Typography
+          variant="h5"
+          marginBottom={"1%"}
+        >
+          You will be redirected in {redirectTime} second{redirectTime > 1? "s" : ""}.
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={redirectToHome}
+        >
+          Log out
+        </Button>
+
+      </CardContent>
+    </AuthCard>
+  );
+
+}
+
+export default Logout;

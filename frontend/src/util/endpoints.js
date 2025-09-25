d303f286394827b244eb58b4cceff131211c8782
Joseph9720 <Josephj9720@gmail.com>
2025-09-24 23:12:31 -0400

    feat: added Logout endpoint to api endpoints
    - The Logout endpoint was added to the api endpoints so that
      request can be made to the backend api to logout the user.

--- frontend/src/util/endpoints.js
+++ frontend/src/util/endpoints.js
@@ -3,6 +3,7 @@ const AUTH = "/auth";
 export const API_ENDPOINTS = {
   REGISTER: AUTH + "/register",
   LOGIN: AUTH + "/login",
+  LOGOUT: AUTH + "/logout",
   REFRESH: AUTH + "/refresh",
   ACCOUNTS : "/accounts",
   TRANSACTIONS : "/transactions",

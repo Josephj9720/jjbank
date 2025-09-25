68cadb7788f51ac1a2a70bd8c5de21043ab31a32
Joseph9720 <Josephj9720@gmail.com>
2025-09-25 03:46:19 -0400

    feat: added isAuthenticating state
    - The isAuthenticating state has been added to the
      useRefreshToken hook. It is initially set to true.
      So now while the app is looking for the authentication
      details, isAuthenticating is true, once the AuthDetails
      are set (actual values, or null), then isAuthenticating
      is set to false.
    
    - This was implemented to prevent race conditions when
      the logout request and refresh request are sent at the
      same time for the logout route. This can also be useful
      to prevent any race condition due to authentication.
    
    - The isAuthenticating state is used to conditionally
      render the children of the AuthenticationProvider
      when the isAuthenticating state is false. This means
      that once the authentication details are known to be
      present or null, all other operations can proceed.
      This prevents race conditions related to authentication.
    
    - Minor fix: setAuthDetails has been removed from the
      dependency array for the refreshToken callBack
      because react ensures that the method to set state
      will remain unchanged.

--- frontend/src/components/AuthenticationProvider.jsx
+++ frontend/src/components/AuthenticationProvider.jsx
@@ -1,10 +1,11 @@
 import { AuthenticationContext, useRefreshToken } from "../hooks/useAuthentication";
+import { CircularProgress } from "@mui/material";
 
 //holds the state for the authentication data, initially blank
 export const AuthenticationProvider = ({ children }) => {
 
   //call hook unconditionally in provider to refresh token globally
-   const [authDetails, setAuthDetails] = useRefreshToken();
+   const [authDetails, setAuthDetails, isAuthenticating] = useRefreshToken();
 
   //value given to the context provider
   //it contains the authentication details, and the method to update the details
@@ -15,7 +16,7 @@ export const AuthenticationProvider = ({ children }) => {
 
   return (
     <AuthenticationContext.Provider value={value}>
-      {children}
+      {!isAuthenticating && children}
     </AuthenticationContext.Provider>
   );
 }

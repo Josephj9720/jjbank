daf46635ef95bfc36d73fb244f02760ebec6383a
Joseph9720 <Josephj9720@gmail.com>
2025-09-30 00:20:21 -0400

    feat: created a custom NavbarLink
    - NavbarLink.jsx is the custom link for the navbar
      of the application. It uses the Link component from
      MUI and sets its component prop as the Link component
      from React-Router. This allows to keep the styling of
      MUI while also benifiting from the client-side navigation
      without page reload of React-Router.
    
    - NavbarLink has 4 props, icon, text, destination and isActive.
    
    - icon is renamed IconComponent so that it respects the convention
      for naming react components. Then the IconComponent is rendered
      only if the IconComponent exists, or in other words, if the icon
      prop was set.
    
    - text is simply the prop for the text for the NavbarLink
    
    - destination is the route where the NavbarLink navigates
    
    - isActive is a boolean which is true whenever the destination
      of the NavbarLink and the current location of the app are the
      same.
    
    - Two set of styles are created for the NavbarLink:
      inactiveStyles and activeStyles
    
    - The proper style is chosen depending on the isActive prop.

---
+++ frontend/src/components/NavbarLink.jsx
@@ -0,0 +1,49 @@
+import { Link, Typography } from "@mui/material";
+import { Link as ReactRouterLink } from "react-router";
+
+const NavbarLink = ({ icon: IconComponent, text, destination, isActive }) => {
+
+  const inactiveStyles = {
+    color: (theme) => theme.palette.navbar.text,
+    display: "flex",
+    alignItems: "center",
+    gap: "1",
+    paddingX: "15px",
+    textAlign: "center",
+    textDecoration: "none",
+    ":hover": {
+      color: (theme) => theme.palette.primary.main,
+      backgroundColor: (theme) => theme.palette.navbar.text
+    },
+    ":focus": {
+      color: (theme) => theme.palette.navbar.text,
+      backgroundColor: (theme) => theme.palette.primary.main,
+      borderBottom: (theme) => `4px solid ${theme.palette.navbar.text}`
+    }
+  };
+
+  const activeStyles = {
+    color: (theme) => theme.palette.navbar.text,
+    backgroundColor: (theme) => theme.palette.primary.main,
+    borderBottom: (theme) => `4px solid ${theme.palette.navbar.text}`,
+    display: "flex",
+    alignItems: "center",
+    gap: "1",
+    paddingX: "15px",
+    textAlign: "center",
+    textDecoration: "none",
+  };
+
+  return (
+    <Link
+      component={ReactRouterLink}
+      to={destination}
+      sx={ isActive ? activeStyles : inactiveStyles }
+    >
+      {IconComponent && <IconComponent />}
+      <Typography variant="subtitle1">{text}</Typography>
+    </Link>
+  );
+}
+
+export default NavbarLink;

91c679e871746cf51b1aa7017e2647a3be511ba9
Joseph9720 <Josephj9720@gmail.com>
2025-09-30 00:49:45 -0400

    feat: Added login/logout button to Navbar
    - The Toolbar inside of the Navbar was styled through the sx prop
      to alignItems:stretch and use the background color set in the
      application's theme.
    
    - AlignItems was set to strech because it makes the items inside
      of the Navbar fill the height of the Navbar.
    
    - Inside of the Toolbar, there will be NavbarLinks on the right
      and there will be NavbarLinks on the left. So far there is only
      on Box inside of the Toolbar, that was set with marginLeft:auto
      so that the components inside of it are all pushed to the right
      of the Navbar.
    
    - Inside of the Box that is set to be to the extreme right of
      the Toolbar, there is a VericalDivider followed by
      a NavbarLink. The NavbarLink is set conditionally to be
      a link to Logout or a link to Login. If authDetails are
      present or if the current route is Logout, the NavbarLink
      will be the one designed for logout. If the authDetails are
      not present and the current route is not logout, then the
      NavbarLink will be the one designed for login.
    
    - The authDetails are taken from the useAuthContext hook
    
    - A helper method was written to find out if a route is the
      current route of the application. The current location is
      determined using the useLocation hook. The helper method
      has the following signature: isCurrentRoute(route)
    
    - The isActive prop of the NavbarLinks is set using the
      isCurrentRoute(route) helper method. So if the route
      that the NavbarLink points to is the currentRoute, it
      used the activeStyles.
    
    Key Points:
    
      - When a user is logged out, the login NavbarLink is shown,
    
      - When a user is logged in, the logout NavbarLink is shown,
    
      - Each NavbarLink redirect to the proper route: Login or Logout
    
    Closes #40

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,53 @@
-import { Box, AppBar, Toolbar, IconButton } from '@mui/material'
+import { Box, AppBar, Toolbar } from '@mui/material'
+import NavbarLink from './NavbarLink';
+import { Lock, LockOpen } from '@mui/icons-material';
+import { useLocation } from 'react-router';
+import VerticalDivider from './VerticalDivider';
+import { useAuthContext } from '../hooks/useAuthentication';
+import { FRONT_END_ROUTES } from '../util/routes';
 
 export default function Navbar() {
+
+  const location = useLocation();
+
+  const { authDetails } = useAuthContext();
+
+  const isCurrentRoute = (route) => {
+    return route === location.pathname;
+  }
+
   return (
-      
+    
       <Box sx={{flexGrow: 1}}>
         <AppBar position='fixed'>
-          <Toolbar>
-            
+          {/* alignItems strech on the toolbar makes the content fill the height of the toolbar (secondary axis) */}
+          <Toolbar sx={{
+            alignItems: "stretch", 
+            backgroundColor: (theme) => theme.palette.navbar.background}}
+          >
+            <Box sx={{marginLeft: "auto", display: "flex", gap: 1}}>
+
+              <VerticalDivider />
+              {
+                authDetails || isCurrentRoute(FRONT_END_ROUTES.LOGOUT)
+                ?
+                  <NavbarLink
+                    icon={LockOpen}
+                    text={"Logout"}
+                    destination={FRONT_END_ROUTES.LOGOUT}
+                    isActive={isCurrentRoute(FRONT_END_ROUTES.LOGOUT)}
+                  />
+                :
+                  <NavbarLink
+                    icon={Lock}
+                    text={"Login"}
+                    destination={FRONT_END_ROUTES.LOGIN}
+                    isActive={isCurrentRoute(FRONT_END_ROUTES.LOGIN)}
+                  />
+              }
+            </Box>
+
+
           </Toolbar>
         </AppBar>
       </Box>

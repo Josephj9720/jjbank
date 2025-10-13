e2def92d92afe51398613b4c739c2eb424dc537a
Joseph9720 <Josephj9720@gmail.com>
2025-10-13 19:00:39 -0400

    feat: Added endpoint for account creation
    - A new endpoint was made for the creation of new accounts.
      It extracts information from the client as a CreateRequest
      DTO, and optionally uses the Authentication object to retrieve
      the current user principal when the userId is not provided in
      in the request.

--- backend/src/main/java/dev/jordanjoseph/backend/controller/AccountController.java
+++ backend/src/main/java/dev/jordanjoseph/backend/controller/AccountController.java
@@ -1,12 +1,16 @@
 package dev.jordanjoseph.backend.controller;
 
 import dev.jordanjoseph.backend.dto.account.AccountView;
+import dev.jordanjoseph.backend.dto.account.CreateRequest;
 import dev.jordanjoseph.backend.dto.account.MoneyRequest;
+import dev.jordanjoseph.backend.model.UserPrincipal;
 import dev.jordanjoseph.backend.service.AccountService;
 import dev.jordanjoseph.backend.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -43,6 +47,21 @@ public class AccountController {
             @RequestHeader(value = "Idempotency-Key", required = false) String idemKey) {
         AccountView response = transactionService.withdraw(id, request.amount(), idemKey);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> create(
+            Authentication authentication,
+            @RequestBody CreateRequest request) {
+
+        if(request.userId() == null) {
+            UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+            accountService.createForUser(principal.getId(), request.accountType());
+        } else {
+            accountService.createForUser(request.userId(), request.accountType());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }

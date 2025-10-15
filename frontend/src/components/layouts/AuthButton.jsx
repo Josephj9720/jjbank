659cadd913eb7fa989cf4cd4a7c757a937b36b05
Joseph9720 <Josephj9720@gmail.com>
2025-10-15 15:42:21 -0400

    feat: Added styling for the AuthButton component
    - Added styling for the AuthButton.jsx to change the colors based
      on the theme and button variant which is specified as prop.
    
    - The proprs where changed to have a default value for 'type'
      and 'variant'. This allowed to removed most conditional statements.
    
    - The background color is set to the authButton background property
      for the proper variant from the theme object.
    
    - The color is set to the authButton text propery for the proper
      variant from the theme object.
    
    - The background color on hover is set to a darker version of the
      background color specified in the them using the MUI darken
      function for the contained variant. For other variants, it is
      set to a lower opacity using the MUI alpha function for the
      outlined authButton's ripple color.
    
    - The button ripple color is changed throught the MuiTouRipple-child
      class by changing its background color. The background color is
      set to the ripple color defined in the theme for the authButton.

--- frontend/src/components/layouts/AuthButton.jsx
+++ frontend/src/components/layouts/AuthButton.jsx
@@ -1,11 +1,10 @@
-import { Button } from '@mui/material';
+import { alpha, Button, darken } from '@mui/material';
 
-const AuthButton = ({ type, text, variant, onClick }) => {
+const AuthButton = ({ type = "submit", text, variant = "contained", onClick }) => {
   return (
     <Button
-      type={type ? type : "submit"}
-      color="secondary"
-      variant={variant ? variant : "contained"}
+      type={type}
+      variant={variant}
       onClick={onClick}
       size="large"
       sx={{
@@ -20,6 +19,14 @@ const AuthButton = ({ type, text, variant, onClick }) => {
           "md" : "0 1.5%",
         },
         "padding" : "0.80rem",
+        "backgroundColor" : (theme) => theme.palette.authButton[variant].background,
+        "color" : (theme) => theme.palette.authButton[variant].text,
+        ":hover" : {
+          "backgroundColor" : (theme) => variant === "contained" ? darken(theme.palette.authButton.contained.background, 0.16) : alpha(theme.palette.authButton.outlined.ripple, 0.08),
+        },
+        ".MuiTouchRipple-child" : {
+          backgroundColor: (theme) => theme.palette.authButton[variant].ripple,
+        }
       }}
     >
       {text}

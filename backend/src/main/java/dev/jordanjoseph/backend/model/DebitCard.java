d6467825ce8055670e556f3b76309708145647ad
Joseph9720 <Josephj9720@gmail.com>
2025-10-17 18:37:18 -0400

    feat: Created DebitCard entity
    - DebitCard.java is an entity which implements the BankingCard
      interface. It also has an id in addition to the other fields
      required from the BankingCard interface. The id is a UUID.
    
    - The owner field is a User entity. It is fetched lazily because
      user details will not often be derived from the card directly

---
+++ backend/src/main/java/dev/jordanjoseph/backend/model/DebitCard.java
@@ -0,0 +1,76 @@
+package dev.jordanjoseph.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.YearMonth;
+import java.util.UUID;
+
+@Entity
+@Table(name = "debit_cards")
+public class DebitCard implements BankingCard {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String last4;
+
+    private String brand;
+
+    private YearMonth expiry;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User owner;
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String getLast4() {
+        return last4;
+    }
+
+    public void setLast4(String last4) {
+        this.last4 = last4;
+    }
+
+    @Override
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public YearMonth getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(YearMonth expiry) {
+        this.expiry = expiry;
+    }
+
+    @Override
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}

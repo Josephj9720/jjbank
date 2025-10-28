fd48789116454c82565599bda55829d1f44cf25c
Joseph9720 <Josephj9720@gmail.com>
2025-10-28 18:32:29 -0400

    feat: removed unecessary use of DebitCardRepository
    - The CustomUserDetailsService is only used to retrieve the
      user prinicpal through the JwtAuthFilter. The JWTs only
      never store debit card numbers so there is no need to try
      another identifier if the email is not found. The earlier
      changes to supprot the debit card have been removed from
      the CustomUserDetailsService as they had no effect.

--- backend/src/main/java/dev/jordanjoseph/backend/service/CustomUserDetailsService.java
+++ backend/src/main/java/dev/jordanjoseph/backend/service/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package dev.jordanjoseph.backend.service;
 
 import dev.jordanjoseph.backend.model.User;
 import dev.jordanjoseph.backend.model.UserPrincipal;
-import dev.jordanjoseph.backend.repository.DebitCardRepository;
 import dev.jordanjoseph.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,17 +15,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private DebitCardRepository debitCardRepository;
-
     @Override
-    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(identifier)
-                .orElse(
-                        debitCardRepository.findByNumber(identifier)
-                        .orElseThrow(() -> new UsernameNotFoundException("Not found"))
-                        .getOwner()
-                );
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                        .orElseThrow(() -> new UsernameNotFoundException("Not found"));
         return new UserPrincipal(user);
     }
 

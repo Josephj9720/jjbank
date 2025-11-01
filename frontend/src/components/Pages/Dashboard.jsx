77810a1155575bcaedf0d268a93cc833b4747894
Joseph9720 <Josephj9720@gmail.com>
2025-11-01 00:25:26 -0400

    feat: Created the layout for the Dashboard page
    - The layout for the Dashboard.jsx page is a 'welcome banner'
      on which the user is greeted by their full name, followed by
      a section displaying their bank accounts, their balance and
      the total balance for the banking services. The name of the
      user is retrieved from the authDetails, and the account
      information is retrived to a call to the api. The access
      token is required to access the resource and it is taken
      from the authDetails. The access token is sent through
      the authorization header defined in the config object
      for axios.

--- frontend/src/components/Pages/Dashboard.jsx
+++ frontend/src/components/Pages/Dashboard.jsx
@@ -1,17 +1,86 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthentication";
 import { FRONT_END_ROUTES } from "../../util/routes";
-import { Box, useTheme } from "@mui/material";
+import { Box, Divider, Typography, useTheme } from "@mui/material";
 import Banner from "../layouts/Banner";
+import Section from "../layouts/Section";
+import SectionRow from "../layouts/SectionRow";
+import api from "../../util/apiClient";
+import { API_ENDPOINTS } from "../../util/endpoints";
 
 const Dashboard = () => {
-  const [accounts, setAccounts] = useState(null);
+  const [accounts, setAccounts] = useState([]);
 
   const { authDetails } = useAuthContext();
 
+  const MY_ACCOUNTS = "/me";
+
+
+  //memoized version of the method to retrieve accounts
+  const getAccounts = useCallback(() => {
+
+    const config = {
+      headers: { "Authorization": `Bearer ${authDetails.accessToken}` },
+    };
+
+    api.get(API_ENDPOINTS.ACCOUNTS + MY_ACCOUNTS, config)
+    .then(response => {
+      console.log(response.data);
+      setAccounts(response.data);
+
+    })
+    .catch(error => {
+      console.log("Failed to fetch data.", error); //how 
+    });
+
+
+  }, [authDetails]);
+
+  //fetch account data, synch component with external system (backend)
+  useEffect(() => {
+    getAccounts();
+  }, [getAccounts]);
+
   const theme = useTheme();
 
+  const accountListItems = accounts.map(account => 
+    <div key={account.id}>
+      <SectionRow>
+        <Typography
+          variant="body1"
+          color={ theme.palette.section.secondaryText }
+        >
+          <b>JJB {account.type} ACCOUNT </b>
+        </Typography>
+        <Typography
+          variant="body1"
+          color={ theme.palette.section.secondaryText }
+          marginLeft={"1%"}
+        >
+          <i>{account.id}</i>
+        </Typography>
+        <Typography
+          variant="body1"
+          color={ theme.palette.section.secondaryText }
+          sx={{
+            "marginLeft": "auto",
+          }}
+        >
+          {account.balance.toFixed(2)} $
+        </Typography>
+      </SectionRow>
+      <Divider
+        sx={{
+          "margin": "1% 0%",
+        }}
+      />
+    </div>
+  );
 
+  const accountSumOfBalances = accounts.reduce(
+    (sum, currentAccount) => sum + currentAccount.balance,
+    0
+  ).toFixed(2);
 
   
   
@@ -34,6 +103,53 @@ const Dashboard = () => {
         borderWidth={ theme.layout.welcomeBanner.borderWidth }
         margin="0 0 2% 0"      
       />
+
+      <Section
+        sectionColor={ "transparent" }
+        borderColor={ "transparent" }
+        borderWidth={ "0px" }
+        margin={ theme.layout.section.alignment.right }
+      >
+        <Typography
+          variant="h5"
+          color={ theme.palette.section.primaryText }
+        >
+          <b>My Accounts</b>
+        </Typography>
+      </Section>
+
+      <Section
+        sectionColor={ theme.palette.section.background }
+        borderColor={ theme.palette.section.border }
+        borderWidth={ theme.layout.section.borderWidth }
+        margin={ theme.layout.section.alignment.right }
+        padding="1%"
+      >
+        <SectionRow>
+          <Typography
+            variant="body1"
+            color={ theme.palette.section.primaryText }
+          >
+            <b>Banking Services</b>
+          </Typography>
+          <Typography
+            variant="body1"
+            color={ theme.palette.section.primaryText }
+            sx={{
+              "marginLeft": "auto",
+            }}
+          >
+            <b>{ accountSumOfBalances }$</b>
+          </Typography>
+        </SectionRow>
+        <Divider
+          sx={{
+            "margin": "1% 0%",
+            "backgroundColor": theme.palette.section.primaryText,
+          }}
+        />
+        {accountListItems}
+      </Section>
 
     </Box>
   );

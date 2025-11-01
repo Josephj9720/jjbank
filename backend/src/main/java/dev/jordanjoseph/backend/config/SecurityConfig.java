f9f0c5292c9112adbd022bf9c092ac88afb8d13a
Joseph9720 <Josephj9720@gmail.com>
2025-10-31 23:39:07 -0400

    feat: Replaced the Cors configuration
    - The CorsConfig.java file was removed. The configuration
      for CORS was implemented in this file using the WebMvcConfigurer
      and overriding its addCorsMapping method. The problem is that
      Spring Security provides another mechanism to implement CORS
      configurations and the current WebMvcConfigurer seemed to not
      be taken into account.
    
    - The CORS configuration is instead done inside of the SecurityConfig
      file. It is implemented in three steps. The first step was to allow
      all requests that uses the Http OPTIONS method in the security filter
      chain. The second step was to define a UrlBasedCorsConfigurationSource
      and register the cors configuration for the entire backend (/**). The
      final step was to add the previously defined CorsConfigurationSource
      to the filter chain (http.cors()).
    
    - Doing this solved the problems with CORS on the frontend.

--- backend/src/main/java/dev/jordanjoseph/backend/config/SecurityConfig.java
+++ backend/src/main/java/dev/jordanjoseph/backend/config/SecurityConfig.java
@@ -1,20 +1,23 @@
 package dev.jordanjoseph.backend.config;
 
 import dev.jordanjoseph.backend.filter.JwtAuthFilter;
-import dev.jordanjoseph.backend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,12 +32,18 @@ public class SecurityConfig {
     @Autowired
     private RefreshTokenLogoutHandler refreshTokenLogoutHandler;
 
+    @Value("${jjb.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(customizer -> customizer.configurationSource(corsConfigurationSource()))
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(authCustomizer -> authCustomizer
+                        //permit all options request, allows cors preflight (options) requests
+                        .requestMatchers(HttpMethod.OPTIONS, "/**")
+                        .permitAll()
                         .requestMatchers("/api/auth/**", "/api/health")
                         .permitAll()
                         .anyRequest().authenticated()
@@ -51,5 +60,17 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedMethods(List.of("GET", "HEAD", "POST", "PATCH", "OPTIONS"));
+        cors.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        cors.setAllowedHeaders(List.of("Authorization", "*"));
+        cors.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
     }
 }

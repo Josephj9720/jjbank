2eae2fa430821969fc43a3d50224b5ac4c515cd2
Joseph9720 <Josephj9720@gmail.com>
2026-02-23 18:16:37 -0500

    feat: modified addContact method
    - The addContact method that takes no displayName
      parameter has been removed and the addContact method
      that uses this parameter has been modified to assign
      the full name of the recipient as displayName if none
      was provided.
    
    - This changed was done because the previous definition
      of the method expected User objects for the owner and
      recipient. This was not good because it would require
      processing in the controller to retrieve the users from
      the database. Instead, the UserRepository is made a
      private variable of the ContactService and is injected
      through the constructor.
    
    - Now, the method takes the ownerId, the recipientEmail
      and the displayName as parameters. The owner and the
      recipient are retrieved from the database using the
      UserRepository.

--- backend/src/main/java/dev/jordanjoseph/backend/service/ContactService.java
+++ backend/src/main/java/dev/jordanjoseph/backend/service/ContactService.java
@@ -1,16 +1,19 @@
 package dev.jordanjoseph.backend.service;
 
 import dev.jordanjoseph.backend.dto.contact.ContactView;
+import dev.jordanjoseph.backend.exception.BusinessException;
 import dev.jordanjoseph.backend.exception.ResourceAlreadyExistsException;
 import dev.jordanjoseph.backend.model.Contact;
 import dev.jordanjoseph.backend.model.User;
 import dev.jordanjoseph.backend.repository.ContactRepository;
+import dev.jordanjoseph.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -18,15 +21,23 @@ public class ContactService {
 
     private final ContactRepository contactRepository;
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public ContactService(ContactRepository contactRepository) {
+    public ContactService(ContactRepository contactRepository, UserRepository userRepository) {
         this.contactRepository = contactRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
-    public void addContact(User owner, User recipient, String displayName) {
+    public void addContact(UUID ownerId, String recipientEmail, String displayName) {
 
-        UUID ownerId = owner.getId();
+        User owner = userRepository.findById(ownerId)
+                .orElseThrow(() -> new NoSuchElementException("The user for which the contact would be created doesn't exist."));
+
+        User recipient = userRepository.findByEmail(recipientEmail)
+                .orElseThrow(() -> new NoSuchElementException("The recipient is not a user of JJBank."));
+
         UUID recipientId = recipient.getId();
 
         if(ownerId.equals(recipientId)) {
@@ -34,7 +45,11 @@ public class ContactService {
         }
 
         if(contactRepository.existsByOwnerIdAndRecipientId(ownerId, recipientId)) {
-            throw new ResourceAlreadyExistsException("Contact", "recipientId", recipient.getId().toString());
+            throw new ResourceAlreadyExistsException("Contact", "recipientId", recipientId.toString());
+        }
+
+        if(displayName.isBlank()) {
+            displayName = recipient.getFullName();
         }
 
         Contact contact = new Contact();
@@ -42,11 +57,6 @@ public class ContactService {
         contact.setRecipient(recipient);
         contact.setDisplayName(displayName);
         contactRepository.save(contact);
-    }
-
-    @Transactional
-    public void addContact(User owner, User recipient) {
-        addContact(owner, recipient, recipient.getFullName());
     }
 
     @Transactional

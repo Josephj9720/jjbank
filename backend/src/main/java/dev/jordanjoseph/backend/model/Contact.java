e1e26a83b02f771115db1b5d6229e3159fed9293
Joseph9720 <Josephj9720@gmail.com>
2026-02-23 01:16:08 -0500

    feat: created Contact entity
    - The Contact entity was created because it would be
      necessary to save the contacts to which users could
      send or request transfers from.
    
    - The Contact entity is simple. It has 4 fields:
      id, owner, recipient, displayName.
    
    - The owner and recipient are saved in the database
      as owner_id, and recipient_id
    
    - The displayName is the name chosen by the user
      to identify the contact. It can be anything they
      want.

---
+++ backend/src/main/java/dev/jordanjoseph/backend/model/Contact.java
@@ -0,0 +1,52 @@
+package dev.jordanjoseph.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
+
+    private String displayName;
+
+    /** set by DB */
+    public UUID getId() {
+        return id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}

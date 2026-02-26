9c8b82a7240487353f0eee640669b4db4c5a4a67
Joseph9720 <Josephj9720@gmail.com>
2026-02-25 21:17:51 -0500

    feat: added missing annotation
    - The @Repository annotaion was added to the
      ContactRepository. It was done for consistency
      with the other repository interfaces in the project
      and also to make it clear that it is managed by
      Spring even though extending JpaRepository already
      makes it a bean.

--- backend/src/main/java/dev/jordanjoseph/backend/repository/ContactRepository.java
+++ backend/src/main/java/dev/jordanjoseph/backend/repository/ContactRepository.java
@@ -4,10 +4,12 @@ import dev.jordanjoseph.backend.model.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.UUID;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, UUID> {
     Page<Contact> findByOwnerId(UUID ownerId, Pageable pageable);
     boolean existsByOwnerIdAndRecipientId(UUID ownerId, UUID recipientId);

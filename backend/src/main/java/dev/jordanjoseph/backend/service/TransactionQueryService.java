940e0f4ae14afa956bf05147ee4d09bde3a74ac8
Joseph9720 <Josephj9720@gmail.com>
2026-02-28 19:29:07 -0500

    feat: modified getDateFromInstant method
    - The getDateFromInstant method in the TransactionQueryService was
      modified to use the InstantToDateConverter utility which is a class
      designed to convert Instant objects to Strings that represent a date.

--- backend/src/main/java/dev/jordanjoseph/backend/service/TransactionQueryService.java
+++ backend/src/main/java/dev/jordanjoseph/backend/service/TransactionQueryService.java
@@ -7,15 +7,13 @@ import dev.jordanjoseph.backend.model.Transaction;
 import dev.jordanjoseph.backend.repository.AccountRepository;
 import dev.jordanjoseph.backend.repository.TransactionRepository;
 import dev.jordanjoseph.backend.util.AccountGuard;
+import dev.jordanjoseph.backend.util.InstantToDateConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
@@ -160,13 +158,8 @@ public class TransactionQueryService {
     }
 
     private String getDateFromInstant(Instant instant) {
-        LocalDate localDate = LocalDate.ofInstant(instant, ZoneId.systemDefault());
-
-        //format date (EE, MMM dd, yyyy)
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE, MMM dd, yyyy");
-        localDate.format(formatter);
-
-        return localDate.toString();
+        InstantToDateConverter converter = new InstantToDateConverter();
+        return converter.toAbbreviatedFullDate(instant);
     }
 
 }

c9d55cc1ecff90a6ef63c97dd3b723a0bb8cbaa1
Joseph9720 <Josephj9720@gmail.com>
2026-03-21 17:22:41 -0400

    feat: added onTransferExpired method
    - In the TransferEventListener, a new method was added
      for events 'onTransferExpired'. The method is annotated
      with @TransactionalEventListener so that it can be triggered
      whenever a TransferExpiredEvent is fired inside of a
      @Transactional method.
    
    - The method uses a switch that depends on the 'emailTo'
      String from the event object. There are three cases
      for the switch expression. The first case is when the
      value of the 'emailTo' string is equal to 'sender'. This
      means that the email is to be sent to the sender of the
      transfer. The second case is when the string is equal to
      'recipient'. This means that the email is to be sent to the
      recipient of the transfer. The third case is the default case.
      Whenever the 'emailTo' string has a value other than 'sender' or
      'recipient', an IllegalStateException is thrown because the
      value is unexpected.

--- backend/src/main/java/dev/jordanjoseph/backend/infra/TransferEventListener.java
+++ backend/src/main/java/dev/jordanjoseph/backend/infra/TransferEventListener.java
@@ -1,6 +1,7 @@
 package dev.jordanjoseph.backend.infra;
 
 import dev.jordanjoseph.backend.dto.email.EmailContent;
+import dev.jordanjoseph.backend.dto.transfer.event.TransferExpiredEvent;
 import dev.jordanjoseph.backend.dto.transfer.event.TransferInitiatedEvent;
 import dev.jordanjoseph.backend.service.EmailTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,42 @@ public class TransferEventListener {
                 emailContent.subject(),
                 emailContent.textContent(),
                 emailContent.htmlContent());
+    }
+
+    @TransactionalEventListener
+    public void onTransferExpired(TransferExpiredEvent event) {
+        switch (event.emailTo().toLowerCase()) {
+            case "sender" -> {
+                EmailContent emailContent = emailTemplateService.senderTransferExpired(
+                        event.senderFullName(),
+                        event.date(),
+                        event.amount(),
+                        event.recipientDisplayName(),
+                        event.reference());
+
+                emailSender.sendFromNoReply(
+                        event.email(),
+                        emailContent.subject(),
+                        emailContent.textContent(),
+                        emailContent.htmlContent());
+            }
+            case "recipient" -> {
+                EmailContent emailContent = emailTemplateService.recipientTransferExpired(
+                        event.recipientDisplayName(),
+                        event.date(),
+                        event.amount(),
+                        event.senderFullName(),
+                        event.reference());
+
+                emailSender.sendFromNoReply(
+                        event.email(),
+                        emailContent.subject(),
+                        emailContent.textContent(),
+                        emailContent.htmlContent());
+            }
+            default -> throw new IllegalStateException("Expired Transfer Event - Unexpected value for 'emailTo': " + event.emailTo());
+
+        }
     }
 
 }

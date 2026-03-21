33b9013c15b05050b58148a7b77da49d71f2d51e
Joseph9720 <Josephj9720@gmail.com>
2026-03-21 11:54:00 -0400

    feat: create transfer initiated event dto
    - An DTO representing an event was created to be used
      whenever a transfer is initiated. This will be used with
      an event listener which has not been created yet.
    
    - The idea is that it contains all the information necessary
      to send an email to the recipient to which a transfer has
      been sent.
    
    - This is useful because the goal is to remove the email
      sending section of any methods in the application that
      is annotated with @Transactional. These methods work with
      the database and rollback if there is an error. The issue
      is that they could send the emails even if the method has
      not completed the task successfully.
    
    - Using an event listener will solve the problem and this
      new event DTO is for the specific case where a transfer
      has been initiated by a User.

---
+++ backend/src/main/java/dev/jordanjoseph/backend/dto/transfer/event/TransferInitiatedEvent.java
@@ -0,0 +1,11 @@
+package dev.jordanjoseph.backend.dto.transfer.event;
+
+public record TransferInitiatedEvent(
+        @jakarta.validation.constraints.Email String recipientEmail,
+        String recipientDisplayName,
+        String date,
+        String amount,
+        String senderFullName,
+        String sharedRef,
+        String transferLink
+) {}

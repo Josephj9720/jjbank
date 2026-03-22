29056f7626a4baed89c1001788f44c0e69b75da6
Joseph9720 <Josephj9720@gmail.com>
2026-03-21 20:30:46 -0400

    feat: replaced email sending process with event
    - The @Transactional methods that send emails in the
      ExternalTransferService have been modified to fire events
      instead. This is done to prevent sending emails when there
      was a rollback.
    
    - The private methdos to generate EmailContent DTOs were removed
      entirely. It is not ideal to use them that way because it makes more
      sense to process the ExternalTransfer entities with PENDING status
      in pairs. This is because the contact information of the recipient
      of the transfer is stored in the TransferToken which is linked to the
      incoming ExternalTransfer and not the outgoing one. All of the
      information required to create the 'TransferExpiredEvent' objets are
      available when the pair of ExternalTransfers are processed together.
    
    - This also fixes the issue with the previous implementation that
      caused the email to be sent twice to the sender: once from the
      sender's perspective and once from the recipient's perspective.
      The sender's account was used while the recipient did not create their
      account so that the sender essentially sends the funds to themselves
      which is the same as not sending anything. And the method to expire
      the pending external transfers was designed to send an email for every
      record of the transaction (sender vs recipient), the email was sent
      twice to the sender. Now, a 'TransferExpiredEvent' is fired one time
      for a sender, but optionally for a recipient on the condition that
      they have created an account with JJBank. This solves the problem of
      sending both emails to the sender.

--- backend/src/main/java/dev/jordanjoseph/backend/service/ExternalTransferService.java
+++ backend/src/main/java/dev/jordanjoseph/backend/service/ExternalTransferService.java
@@ -1,15 +1,12 @@
 package dev.jordanjoseph.backend.service;
 
-import dev.jordanjoseph.backend.dto.email.EmailContent;
-import dev.jordanjoseph.backend.infra.EmailSender;
-import dev.jordanjoseph.backend.model.Account;
-import dev.jordanjoseph.backend.model.ExternalTransfer;
-import dev.jordanjoseph.backend.model.TransferToken;
+import dev.jordanjoseph.backend.dto.transfer.event.TransferExpiredEvent;
+import dev.jordanjoseph.backend.model.*;
 import dev.jordanjoseph.backend.repository.ExternalTransferRepository;
 import dev.jordanjoseph.backend.repository.TransferTokenRepository;
 import dev.jordanjoseph.backend.util.InstantToDateConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +22,7 @@ public class ExternalTransferService {
 
     private TransferTokenRepository transferTokenRepository;
 
-    private EmailSender emailSender;
-
-    private EmailTemplateService emailTemplateService;
+    private ApplicationEventPublisher eventPublisher;
 
     private InstantToDateConverter instantToDateConverter;
 
@@ -35,33 +30,92 @@ public class ExternalTransferService {
     public ExternalTransferService(
             ExternalTransferRepository externalTransferRepository,
             TransferTokenRepository transferTokenRepository,
-            EmailSender emailSender,
-            EmailTemplateService emailTemplateService) {
+            ApplicationEventPublisher eventPublisher) {
 
         this.externalTransferRepository = externalTransferRepository;
         this.transferTokenRepository = transferTokenRepository;
-        this.emailSender = emailSender;
-        this.emailTemplateService = emailTemplateService;
+        this.eventPublisher = eventPublisher;
         this.instantToDateConverter = new InstantToDateConverter();
     }
 
     @Transactional
     public void expirePendingTransfers() {
-        externalTransferRepository.findByStatusAndExpiresAtBefore(ExternalTransfer.Status.PENDING, Instant.now())
-                .forEach(externalTransfer -> {
-                    externalTransfer.setStatus(ExternalTransfer.Status.EXPIRED);
+        externalTransferRepository.findByStatusAndTypeAndExpiresAtBefore(ExternalTransfer.Status.PENDING, Transaction.Type.TRANSFER_OUT,Instant.now())
+                .forEach(outgoing -> {
+                    ExternalTransfer incoming = getComplementaryTransfer(outgoing.getReference(), outgoing.getAccount().getId(), outgoing.getId());
 
-                    //send message to user
-                    EmailContent emailContent = switch (externalTransfer.getType()) {
-                        case TRANSFER_IN -> expirePendingIncomingTransfer(externalTransfer);
-                        case TRANSFER_OUT -> expirePendingOutgoingTransfer(externalTransfer);
-                        default -> throw new IllegalStateException("Unexpected value: " + externalTransfer.getType());
-                    };
-                    emailSender.sendFromNoReply(
-                            externalTransfer.getAccount().getUser().getEmail(),
-                            "Transfer  Expired - " + externalTransfer.getReference(),
-                            emailContent.textContent(),
-                            emailContent.htmlContent()
+                    //expire the external transfers
+                    outgoing.setStatus(ExternalTransfer.Status.EXPIRED);
+                    incoming.setStatus(ExternalTransfer.Status.EXPIRED);
+
+                    //refund sender
+                    Account senderAccount = outgoing.getAccount();
+                    senderAccount.setBalance(senderAccount.getBalance().add(outgoing.getAmount()));
+
+                    //invalidate
+
+                    //get sender and recipient account IDs
+                    UUID senderAccountId = outgoing.getAccount().getId();
+                    UUID recipientAccountId = incoming.getAccount().getId();
+
+                    //set up variable to hold recipient name and email
+                    String recipientName;
+                    String recipientEmail;
+
+                    //get TransferToken
+                    TransferToken token = transferTokenRepository.findByIncomingTransferId(incoming.getId())
+                            .orElseThrow(() ->
+                                    new NoSuchElementException("Transfer Token could not be found for external transfer id: " + incoming.getId()));
+
+                    if(senderAccountId.equals(recipientAccountId)) {
+                        //the recipient has not registered an account with JJBank, delete their record of the transaction
+                        //only need the record for the sender who is a JJBank user
+                        transferTokenRepository.delete(token);
+                        externalTransferRepository.delete(incoming);
+
+                        //set recipient name and email
+                        recipientName = token.getRecipient().getDisplayName();
+                        recipientEmail = token.getRecipient().getRecipientEmail();
+
+                    } else {
+                        //the recipient is a User of JJBank, keep the record, invalidate transfer token
+                        token.setInvalid(true);
+
+                        //get recipient User
+                        User recipient = incoming.getAccount().getUser();
+
+                        //set recipient name and email
+                        recipientName = recipient.getFullName();
+                        recipientEmail = recipient.getEmail();
+                    }
+
+                    //get sender User
+                    User sender = outgoing.getAccount().getUser();
+
+                    //retrieve necessary information for events
+                    String expiryDate = instantToDateConverter.toAbbreviatedFullDate(outgoing.getExpiresAt());
+                    String amount = outgoing.getAmount().toPlainString();
+                    String sharedRef = outgoing.getReference();
+
+                    //fire events
+                    eventPublisher.publishEvent(new TransferExpiredEvent(
+                            "recipient",
+                            recipientEmail,
+                            recipientName,
+                            expiryDate,
+                            amount,
+                            sender.getFullName(),
+                            sharedRef)
+                    );
+
+                    eventPublisher.publishEvent(new TransferExpiredEvent(
+                            "sender",
+                            sender.getEmail(),
+                            recipientName,
+                            expiryDate,
+                            amount,
+                            sender.getFullName(),
+                            sharedRef)
                     );
 
                 });
@@ -74,62 +128,6 @@ public class ExternalTransferService {
                     //send message to recipient
                     externalTransfer.setReminderSent(true);
                 });
-    }
-
-    private EmailContent expirePendingIncomingTransfer(ExternalTransfer incoming) {
-        //get complementary outgoing transfer and sender/recipient account IDs
-        ExternalTransfer outgoing = getComplementaryTransfer(incoming.getReference(), incoming.getAccount().getId(), incoming.getId());
-        UUID senderAccountId = outgoing.getAccount().getId();
-        UUID recipientAccountId = incoming.getAccount().getId();
-
-        TransferToken token = transferTokenRepository.findByIncomingTransferId(incoming.getId())
-                .orElseThrow(() ->
-                        new NoSuchElementException("Transfer Token could not be found for external transfer id: " + incoming.getId()));
-
-        if(senderAccountId.equals(recipientAccountId)) {
-            //the recipient has not registered an account with JJBank, delete their record of the transaction
-            //only need the record for the sender who is a JJBank user
-            transferTokenRepository.delete(token);
-            externalTransferRepository.delete(incoming);
-
-        } else {
-            //invalidate transfer token
-            token.setInvalid(true);
-        }
-
-        //get sender/recipient names
-        String sender = outgoing.getAccount().getUser().getFullName();
-        String recipient = incoming.getAccount().getUser().getFullName();
-
-        //generate email templates
-        return emailTemplateService.recipientTransferExpired(
-                recipient,
-                instantToDateConverter.toAbbreviatedFullDate(incoming.getCreatedAt()),
-                incoming.getAmount().toPlainString(),
-                sender,
-                incoming.getReference()
-        );
-    }
-
-    private EmailContent expirePendingOutgoingTransfer(ExternalTransfer outgoing) {
-        //get complementary ingoing transfer and sender/recipient names
-        ExternalTransfer incoming = getComplementaryTransfer(outgoing.getReference(), outgoing.getAccount().getId(), outgoing.getId());
-        String recipient = incoming.getAccount().getUser().getFullName();
-        String sender = outgoing.getAccount().getUser().getFullName();
-
-        //return funds to sender
-        Account senderAccount = outgoing.getAccount();
-        senderAccount.setBalance(senderAccount.getBalance().add(outgoing.getAmount()));
-
-
-        //generate email templates
-        return emailTemplateService.senderTransferExpired(
-                sender,
-                instantToDateConverter.toAbbreviatedFullDate(outgoing.getCreatedAt()),
-                outgoing.getAmount().toPlainString(),
-                recipient,
-                outgoing.getReference()
-        );
     }
 
     private ExternalTransfer getComplementaryTransfer(String reference, UUID accountId, UUID externalTransferId) {

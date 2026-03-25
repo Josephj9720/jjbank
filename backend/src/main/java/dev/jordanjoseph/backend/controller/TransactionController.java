9abe8c5579f067537e118154485ffc8d317db27e
Joseph9720 <Josephj9720@gmail.com>
2026-03-25 02:35:41 -0400

    feat: added enpoint to claim external transfer
    - A method was added to the TransactionController to provide an endpoint
      to claim external transfers. It takes an Authentication object,
      a ClaimExternalTransferRequest from the @RequestBody,
      and an optional idemKey String from the @RequestHeader. The method
      returns a ResponseEntity<String>.
    
    - It uses the Authentication object to obtain the UserPrincipal which
      in turn is used t obtain the 'username' of the currently logged in
      user which is their email address.
    
    - The username (email), the request and the idemKey are then used to
      call the 'claimExternalTransfer' method from the transaction service,
      which returns an ApiResult. This result is used to send the proper
      response based on SUCCESS or FAILURE of the attempt to claim
      the external transfer.
    
    - When successful, the HTTP status is OK, and when it fails,
      the HTTP status is BAD_REQUEST. In both cases, the message
      String is sent with the ResponseEntity.

--- backend/src/main/java/dev/jordanjoseph/backend/controller/TransactionController.java
+++ backend/src/main/java/dev/jordanjoseph/backend/controller/TransactionController.java
@@ -1,14 +1,18 @@
 package dev.jordanjoseph.backend.controller;
 
+import dev.jordanjoseph.backend.dto.common.ApiResult;
+import dev.jordanjoseph.backend.dto.transfer.ClaimExternalTransferRequest;
 import dev.jordanjoseph.backend.dto.transfer.InternalTransferRequest;
 import dev.jordanjoseph.backend.dto.transfer.InternalTransferResponse;
 
 import dev.jordanjoseph.backend.dto.transfer.OutgoingExternalTransferRequest;
+import dev.jordanjoseph.backend.model.UserPrincipal;
 import dev.jordanjoseph.backend.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,5 +36,26 @@ public class TransactionController {
             @RequestHeader(value = "Idempotency-Key", required = false) String idemKey) {
         transactionService.initiateExternalTransfer(request, idemKey);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/external/transfer/claim")
+    public ResponseEntity<String> claimExternalTransfer(
+            Authentication authentication,
+            @RequestBody ClaimExternalTransferRequest request,
+            @RequestHeader(value = "Idempotency-Key", required = false) String idemKey) {
+
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        ApiResult result = transactionService.claimExternalTransfer(
+                principal.getUsername(),
+                request,
+                idemKey);
+
+        if(result.status().equals(ApiResult.Status.SUCCESS)) {
+            return ResponseEntity.ok(result.message());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(result.message());
+        }
     }
 }

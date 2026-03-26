ac9bb4968503239003b584a8519d4393112808d4
Joseph9720 <Josephj9720@gmail.com>
2026-03-25 22:45:03 -0400

    feat: added String and enum for 'action'
    _ An 'Action' enum with two values (CLAIM, DECLINE) was
      added to the ClaimOrDeclineExternalTransferRequest
      DTO. A String 'action' was also added to it as part
      of the fields that compose the record.
    
    - This was done to differentiate between the exact
      action chosen by the User.

--- backend/src/main/java/dev/jordanjoseph/backend/dto/transfer/ClaimOrDeclineExternalTransferRequest.java
+++ backend/src/main/java/dev/jordanjoseph/backend/dto/transfer/ClaimOrDeclineExternalTransferRequest.java
@@ -3,7 +3,10 @@ package dev.jordanjoseph.backend.dto.transfer;
 import java.util.UUID;
 
 public record ClaimOrDeclineExternalTransferRequest(
+    String action,
     String transferToken,
     String securityAnswer,
     UUID recipientAccountId
-) {}
+) {
+    public enum Action { CLAIM, DECLINE }
+}
